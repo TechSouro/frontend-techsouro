3cc01f742f8022771faeb6421a737577d542dd6d
Renan César <90277714+renancorreadev@users.noreply.github.com>
2023-12-06 17:42:32 -0300

    fix: change api url because server down

--- src/provider/Api.ts
+++ src/provider/Api.ts
@@ -3,7 +3,7 @@ import https from "https";
 
 export const Api = axios.create({
   baseURL:
-    "https://aabe-2804-431-cfef-80ae-6dd8-81e0-8ec1-7ffd.ngrok-free.app/api",
+    "https://b71b-2804-431-cfef-80ae-9b1-5617-a4cc-8a7a.ngrok-free.app",
   httpsAgent: new https.Agent({ rejectUnauthorized: false }),
   headers: {
     "ngrok-skip-browser-warning": "*",
